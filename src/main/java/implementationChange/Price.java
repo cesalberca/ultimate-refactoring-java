1f9efc29d03b1ac4a367571bc0bc1f5457e2f473
César Alberca <cesalberca@gmail.com>
2019-03-12 11:47:02 +0100

    Refactor using object price

---
+++ src/main/java/implementationChange/Price.java
@@ -0,0 +1,13 @@
+package implementationChange;
+
+public class Price {
+    private final int value;
+
+    public Price(int price) {
+        value = price;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
--- src/main/java/implementationChange/ShoppingCart.java
+++ src/main/java/implementationChange/ShoppingCart.java
@@ -1,21 +1,23 @@
 package implementationChange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
-    private int price;
+    private List<Price> prices = new ArrayList<>();
 
-    public void add(int price) {
-        this.price = price;
+    public void add(Price price) {
+        this.prices.add(price);
     }
 
-    public int calculateTotalPrice() {
-        return price;
+    public Price calculateTotal(){
+        return new Price(prices.stream().mapToInt(p->p.getValue()).sum());
     }
-
     public boolean hasDiscount() {
-        return price >= 100;
+        return calculateTotal().getValue() >= 100;
     }
 
     public int numberOfProducts() {
-        return 1;
+        return prices.size();
     }
 }
