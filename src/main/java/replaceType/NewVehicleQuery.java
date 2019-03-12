3759da1f57717b397b404263e3bc4c6f1d07b8d8
César Alberca <cesalberca@gmail.com>
2019-03-12 16:27:14 +0100

    Add todo

--- src/main/java/replaceType/NewVehicleQuery.java
+++ src/main/java/replaceType/NewVehicleQuery.java
@@ -3,6 +3,7 @@ package replaceType;
 public class NewVehicleQuery {
     public String brand;
     public String description;
+    // TODO Remove this field
     public String brandDescription;
 
     public NewVehicleQuery(String brand, String description) {
