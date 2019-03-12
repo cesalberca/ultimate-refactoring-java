930597105551462401c2fdc2ea37d1dca58fb942
César Alberca <cesalberca@gmail.com>
2019-03-12 17:30:58 +0100

    Refactor to streams

--- src/main/java/pipelines/Author.java
+++ src/main/java/pipelines/Author.java
@@ -6,22 +6,19 @@ Refactoring to pipelines by Martin Fowler:
 https://martinfowler.com/articles/refactoring-pipelines.html
  */
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Author {
     public String company;
     public String twitterHandle;
 
     static public List<String> twitterHandles(List<Author> authors, String company) {
-        List<String> result = new ArrayList();
-        for (Author a: authors) {
-            if (a.company == company) {
-                String handle = a.twitterHandle;
-                if (handle != null)
-                    result.add(handle);
-            }
-        }
-        return result;
+        return authors.stream()
+                .filter(a -> a.company.equals(company))
+                .map(a -> a.twitterHandle)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 }
